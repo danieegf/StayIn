04fa27233bc607388fbd5522845e8057f5bf9f2c
José Roberto Mejía Pacheco <isscjrmpacheco@gmail.com>
2021-12-08 10:29:34 -0600

    Corrección de Perfil
    Corrección de perfil con campos suplicados  por mantener ambos cambios de versión

---
+++ src/app/models/comment.model.ts
@@ -0,0 +1,9 @@
+export class Comment {
+    id_Comentario?:Number;
+    comentario?:string;
+    descripcion?:string;
+    calificacion?:Number;
+    id_Sitio?:Number;
+    id?:Number;
+    fecha?:Date;
+}
--- src/services/comment.stayiservice.service.ts
+++ src/services/comment.stayiservice.service.ts
@@ -1,10 +1,13 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 
+const baseURL = 'https://stayinsafe-api.azurewebsites.net';
 @Injectable({
   providedIn: 'root'
 })
 export class CommentStayiserviceService {
+  
   // Http Options
   httpOptions = {
     headers: new HttpHeaders({
@@ -15,7 +18,23 @@ export class CommentStayiserviceService {
     }),
   };
   constructor(private httpClient: HttpClient) { }
+  createSite(comment: Comment): Observable<Comment> {
+    console.log(JSON.stringify(comment));
+    return (
+      this.httpClient
+        .post<Comment>(
+          baseURL + '/api/Comentarios/AddComment',
+          JSON.stringify(comment),
+          this.httpOptions
+        )
+        .pipe(retry(1), catchError(this.handleError))
+    );
+  }
 
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    console.log(err.message);
+    return throwError(() => err);
+  }
   
 }
 
