030ff9fec8d7955f3ff2d026b15af3d01bb84958
José Roberto Mejía Pacheco <isscjrmpacheco@gmail.com>
2021-12-10 11:02:20 -0600

    Pelfil con Get

--- src/app/components/web/profile/profile.component.ts
+++ src/app/components/web/profile/profile.component.ts
@@ -11,7 +11,7 @@ import { MessageStayiserviceService } from 'src/services/message.stayiservice.se
 })
 export class ProfileComponent implements OnInit {
   user!: User;
-
+  imageSrc: string = '';
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
@@ -24,23 +24,25 @@ export class ProfileComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    console.log('https://stayinsafe-api.azurewebsites.net/api/User/GetUser/'+localStorage.getItem('UserId'))
     this.httpClient
       .get(
-        'https://stayinsafe-api.azurewebsites.net/api/Comentarios/GetComments/1',
+        'https://stayinsafe-api.azurewebsites.net/api/User/GetUser/'+localStorage.getItem('UserId'),
         this.httpOptions
       )
       .subscribe({
         next: (response: any) => {
           console.log(response);
           this.user = new User();
-          this.user.p_Nombre = this.user.p_Nombre;
-          this.user.s_Nombre = this.user.s_Nombre;
-          this.user.apellido_Materno = this.user.apellido_Materno;
-          this.user.apellido_Paterno = this.user.apellido_Paterno;
-          this.user.fecha_Nacimiento = this.user.fecha_Nacimiento;
-          this.user.email = this.user.email;
-          this.user.pass = this.user.pass;
-          this.user.imagen = this.user.imagen;
+          this.user.p_Nombre = response.p_Nombre;
+          this.user.s_Nombre = response.s_Nombre;
+          this.user.apellido_Materno = response.apellido_Materno;
+          this.user.apellido_Paterno = response.apellido_Paterno;
+          this.user.fecha_Nacimiento = response.fecha_Nacimiento;
+          this.user.email = response.email;
+          this.user.imagen = response.imagen;
+          this.user.telefono = response.telefono;
+          this.imageSrc = response.imagen;
         },
         error: (e) => this.messageService.error(),
         complete: () => {},
